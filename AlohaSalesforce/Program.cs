da936fff1b40175a00e343be0b734685c9e227ca
javi.dombro <javi.dombro@outlook.com>
2021-07-16 20:59:07 -0300

    Add project files.

---
+++ AlohaSalesforce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlohaSalesforce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
